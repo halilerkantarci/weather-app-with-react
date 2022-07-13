cc8b2620156f2c6a5eb47ec9694104ca5e6a9c72
halil <halilerk77@icloud.com>
2022-07-13 18:03:52 +0300

    .

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const WeatherCard = () => {
-  return <div>WeatherCard</div>;
+const WeatherCard = ({ data }) => {
+  const { main, sys, name, weather, iconUrl } = data;
+  console.log(data);
+  return (
+    <li className="city">
+      <h2 className="city-name">
+        <span>{name}</span>
+        <sup>{sys.country}</sup>
+      </h2>
+      <div className="city-temp">
+        {Math.round(main.temp)}
+        <sup>°C</sup>
+      </div>
+      <figure>
+        <img className="city-icon" src={iconUrl} alt="city-icon" />
+        <figcaption>{weather[0].description}</figcaption>
+      </figure>
+    </li>
+  );
 };
 
 export default WeatherCard;
