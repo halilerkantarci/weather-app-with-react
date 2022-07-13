2ea5751fd6b0729153cb7c12c6fa62b468162f3c
halil <halilerk77@icloud.com>
2022-07-13 18:57:44 +0300

    finished

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import WeatherCard from "./WeatherCard";
 const Main = () => {
   const [searchText, setSearchText] = useState("");
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     setSearchText(e.target.value);
@@ -27,7 +28,17 @@ const Main = () => {
       const { main, name, sys, weather, id } = response.data;
       const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
 
-      setData([...data, { main, name, sys, weather, id, iconUrl }]);
+      const isExist = data.some((card) => card.id === id);
+      if (isExist) {
+        setError(
+          `You already know the weather for ${name}, Please search for another city 😊`
+        );
+        setTimeout(() => {
+          setError("");
+        }, 5000);
+      } else {
+        setData([{ main, name, sys, weather, id, iconUrl }, ...data]);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -44,7 +55,7 @@ const Main = () => {
           onChange={handleChange}
         />
         <button type="submit">SUBMIT</button>
-        <span className="msg"></span>
+        <span className="msg">{error}</span>
       </form>
       <div className="container">
         <ul className="cities">
